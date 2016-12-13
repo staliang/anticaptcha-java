6d26145800d43b2026779ccf37b91b3bbfe0078d
Alexandr_Badin <Alexandr_Badin@epam.com>
2016-12-13 16:00:02 +0400

    clean code

--- src/main/java/com/anticaptcha/AnticaptchaApiWrapper.java
+++ src/main/java/com/anticaptcha/AnticaptchaApiWrapper.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
-class AnticaptchaApiWrapper {
+public class AnticaptchaApiWrapper {
 
     private static HashMap<String, Boolean> HostsChecked = new HashMap<>();
 
